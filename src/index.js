2b9e42c370762bccedf18c7a7d86c0aaa0150bff
andyw <andyw@ciandt.com>
2020-08-28 10:38:16 +0800

    Add core-js and regenerator-runtime.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// for @babel-polyfill
+// quote from babel doc: As of Babel 7.4.0, this package 
+// has been deprecated in favor of directly including core-js/stable 
+// (to polyfill ECMAScript features) and
+// regenerator-runtime/runtime (needed to use transpiled generator functions):
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 const a = 'a';
 console.log(a);
 
